12e8799498390019b0257d05d9c2fee164a79eca
silva3265 <silva3265@gmail.com>
2024-06-24 15:03:54 -0300

    Convertendo de String para Date

---
+++ src/string_para_datas/Principal.java
@@ -0,0 +1,18 @@
+package string_para_datas;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Principal {
+
+    public static void main(String[] args) throws ParseException {
+        String dataTexto = "30/12/2023 10:20:45";
+        DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); /* vai converter a data da String pro tipo Date */
+
+        Date data = formatador.parse(dataTexto);
+        System.out.println(data);
+    }
+
+}
