2e8e01d2bd155b057bca201549dbfa013e4e015c
silva3265 <silva3265@gmail.com>
2024-06-25 13:00:51 -0300

    Operações de datas com o tipo Calendar

---
+++ src/calendar/OperacoesDatasCalendar.java
@@ -0,0 +1,21 @@
+package calendar;
+
+import java.util.Calendar;
+
+public class OperacoesDatasCalendar {
+	
+	public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.DAY_OF_MONTH, 20);
+
+//        calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+        calendar.add(Calendar.MONTH, 3);
+//        calendar.add(Calendar.MONTH, -3);
+
+        int ultimoDiaDoMes = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        calendar.set(Calendar.DAY_OF_MONTH, ultimoDiaDoMes);
+
+        System.out.println(calendar.getTime());
+    }
+
+}
